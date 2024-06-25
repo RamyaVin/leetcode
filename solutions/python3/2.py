e313e48b304694f92e96cda2197e04ccc2e330db
RB <60027406+RamyaVin@users.noreply.github.com>
2024-06-25 09:38:09 +0530

    Update 2.py

--- solutions/python3/2.py
+++ solutions/python3/2.py
@@ -6,11 +6,34 @@
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        left = 0
-        dummy = cur = ListNode(-1)
-        while l1 or l2 or left:
-            left, sm = divmod(sum(l and l.val or 0 for l in (l1, l2)) + left, 10)
-            cur.next = cur = ListNode(sm)
-            l1 = l1 and l1.next
-            l2 = l2 and l2.next
-        return dummy.next
+        if l1 is None and l2 is None:
+            return None
+
+        current = ListNode()
+        root = current
+        rem = 0
+        prev = None
+
+        while True:
+            if not l1 and not l2:
+                if rem:
+                    sum = ListNode(val=rem)
+                    prev.next = sum
+                return root
+          
+            if prev:  
+                prev.next = current
+                
+            sum = rem
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+    
+            rem = 1 if sum > 9 else 0
+            current.val = sum if sum < 10 else sum - 10
+
+            prev = current
+            current = ListNode()
