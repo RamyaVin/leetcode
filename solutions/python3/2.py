ecfb93c695ed2a9e4c7742df0a7f100bbbc62487
RB <60027406+RamyaVin@users.noreply.github.com>
2024-06-25 10:16:20 +0530

    Update 2.py

--- solutions/python3/2.py
+++ solutions/python3/2.py
@@ -1,16 +1,37 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#Input: l1 = [2,4,3], l2 = [5,6,4]
+#Output: [7,0,8]
+#Explanation: 342 + 465 = 807.
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        left = 0
-        dummy = cur = ListNode(-1)
-        while l1 or l2 or left:
-            left, sm = divmod(sum(l and l.val or 0 for l in (l1, l2)) + left, 10)
-            cur.next = cur = ListNode(sm)
-            l1 = l1 and l1.next
-            l2 = l2 and l2.next
-        return dummy.next
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 is None and l2 is None:
+            return None
+
+        current = ListNode()
+        root = current
+        rem = 0
+        prev = None
+
+        while True:
+            if not l1 and not l2:
+                if rem:
+                    sum = ListNode(val=rem)
+                    prev.next = sum
+                return root
+          
+            if prev:  
+                prev.next = current
+                
+            sum = rem
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+    
+            rem = 1 if sum > 9 else 0
+            current.val = sum if sum < 10 else sum - 10
+
+            prev = current
+            current = ListNode()
