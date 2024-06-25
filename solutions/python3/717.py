6694450126c9ceec58c1bf2eebd9327637336320
RB <60027406+RamyaVin@users.noreply.github.com>
2024-06-25 09:19:09 +0530

    Update 717.py

--- solutions/python3/717.py
+++ solutions/python3/717.py
@@ -1,10 +1,12 @@
-class Solution:
-    def isOneBitCharacter(self, bits):
-        """
-        :type bits: List[int]
-        :rtype: bool
-        """
-        while bits:
-            last=bits.pop(0)
-            if last==1:bits.pop(0)
-        return True if last==0 else False
+class Solution:    
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        while i < len(bits):
+            if bits[i] == 1:
+                i += 2
+                if i > len(bits):
+                    return False
+            else:
+                if i == len(bits)-1:
+                    return True
+                i += 1
