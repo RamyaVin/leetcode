c3869537b96c0c7ebb8d6a9b70118c03bc980303
RB <60027406+RamyaVin@users.noreply.github.com>
2024-06-26 10:44:07 +0530

    Update 3.py

--- solutions/python3/3.py
+++ solutions/python3/3.py
@@ -1,12 +1,13 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        mx, start, chars = 0, 0, {}
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = {}  # Store character indices
+        start = 0  # Start of the sliding window
+        max_length = 0
+
         for i in range(len(s)):
-            if s[i] in chars and start <= chars[s[i]]: start = chars[s[i]] + 1
-            else: mx = max(mx, i - start + 1)
+            if s[i] in chars and chars[s[i]] >= start:
+                start = chars[s[i]] + 1
             chars[s[i]] = i
-        return mx
+            max_length = max(max_length, i - start + 1)
+
+        return max_length
